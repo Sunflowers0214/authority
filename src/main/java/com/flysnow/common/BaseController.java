2b9236f582008c11d3443cb70a149a47853684b5
flysnow <972855727@qq.com>
2018-12-15 20:50:52 +0800

    删除

--- src/main/java/com/flysnow/common/BaseController.java
+++
@@ -1,9 +0,0 @@
-package com.flysnow.common;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class BaseController {
-    protected Logger logger = LoggerFactory.getLogger(this.getClass());
-
-}
--- src/main/java/com/flysnow/common/BaseService.java
+++
@@ -1,9 +0,0 @@
-package com.flysnow.common;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class BaseService {
-    protected Logger logger = LoggerFactory.getLogger(this.getClass());
-
-}
